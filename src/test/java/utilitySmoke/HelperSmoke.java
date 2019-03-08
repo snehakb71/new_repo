266e5a642d098d7a497cce2ef95315b903a01c71
snehakb71 <sr39@sr39-PC>
2019-03-08 12:07:46 +0530

    screenshoot

--- src/test/java/utilitySmoke/HelperSmoke.java
+++ src/test/java/utilitySmoke/HelperSmoke.java
@@ -1,5 +1,23 @@
 package utilitySmoke;
 
+import java.io.File;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
 public class HelperSmoke {
+	
+	
+	
+WebDriver driver;
+	
+	public static void screenShoot(WebDriver driver, String imagename) throws Exception
+	{
+		
+		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File("./ScreenShot/"+imagename+".png"));
+	}
 
 }
